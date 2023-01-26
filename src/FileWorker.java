7374e9172cc6bcf0b0e5dc3727cd09fead616d9f
kholopkin yuriy <holopkin_yurik@mail.ru>
2023-01-26 17:49:35 +0300

    Создал пользовательский интерфейс Немного оптимизировал код

--- src/FileWorker.java
+++ src/FileWorker.java
@@ -1,29 +1,12 @@
 import java.io.*;
 import java.util.ArrayList;
-import java.util.Scanner;
 
 public class FileWorker {
-    private final String fileInput;
-    private final String fileOutput;
     private final ArrayList<String> arrayKiz = new ArrayList<>();
-
-    public FileWorker() {
-        System.out.println("ВВедите файл источник кизов");
-        Scanner scanner = new Scanner(System.in);
-        fileInput = scanner.nextLine();
-        System.out.println("ВВедите файл разобранных кизов");
-        fileOutput = scanner.nextLine();
-        loadFile(fileInput);
-        saveFile(fileOutput);
-    }
     public FileWorker(String inputFile, String outputFile) {
-        this.fileInput = inputFile;
-        this.fileOutput = outputFile;
         loadFile(inputFile);
         saveFile(outputFile);
     }
-
-
     private void loadFile(String fileName) {
         try (BufferedReader reader = new BufferedReader(new FileReader(fileName))) {
             while(reader.ready()) {
@@ -34,7 +17,6 @@ public class FileWorker {
             e.printStackTrace();
         }
     }
-
     private void saveFile(String fileName) {
         try (BufferedWriter writer = new BufferedWriter(new FileWriter(fileName))) {
             writer.write("Тип\tGTIN\tСерийный номер\tКод ответа\tКриптохвост");
